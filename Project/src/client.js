f23d43a28578c97c3f394fcde79a4fedbd97fd67
claudianeo06 <73271990+claudianeo06@users.noreply.github.com>
2024-02-27 17:31:50 +0100

    not tested

---
+++ Project/src/client.js
@@ -0,0 +1,71 @@
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4dnV1Y29oY3JtcGFvc21hcmVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwMzc4NTcsImV4cCI6MjAyNDYxMzg1N30.t9D300Gp5TOIIbYANc9VYtJwM8lgZ42y_4FYfVRbfW4";
+const url = "https://ixvuucohcrmpaosmarep.supabase.co";
+const database = supabase.createClient(url, key);
+
+//dom elements
+const contentX = document.getElementById("x");
+const contentY = document.getElementById("y");
+const contentTime = document.getElementById("time");
+const contentId = document.getElementById("id");
+const contentAlpha = document.getElementById("alpha");
+const contentBeta = document.getElementById("beta");
+const contentGamma = document.getElementById("gamma");
+const button = document.getElementById("accelPermsButton");
+const value = document.getElementById("value");
+const image = document.getElementById("image");
+
+let px = 50; // Position x and y
+let py = 50;
+let vx = 0.0; // Velocity x and y
+let vy = 0.0;
+
+let updateRate = 1 / 60; // Sensor refresh rate
+
+async function updateSupabase(px, py, rotation_degrees, frontToBack_degrees, leftToRight_degrees) {
+    let res = await database
+      .from(tableName)
+      .update({
+        values: {
+          x: px,
+          y: py,
+          alpha: rotation_degrees,
+          beta: frontToBack_degrees,
+          gamma: leftToRight_degrees,
+        },
+        updated_at: new Date(),
+      })
+      .eq("id", id);
+}
+
+let tableName = "accelerometer";
+document.addEventListener("DOMContentLoaded", async () => {
+  //subscribe to changes in the
+  database
+    .channel(tableName)
+    .on(
+      "postgres_changes",
+      { event: "*", schema: "public", table: tableName },
+      (payload) => {
+        // handleInserts(payload.new);
+        handleInserts(payload.new);
+      }
+    )
+    .subscribe();
+
+  //select all data from sensors
+  let { data, error } = await database.from(tableName).select("*");
+  handleInserts(data[0]);
+});
+
+function handleInserts(data) {
+  console.log(data);
+  contentX.innerHTML = data.values.x;
+  contentY.innerHTML = data.values.y;
+  contentTime.innerHTML = data.updated_at;
+  contentId.innerHTML = data.id;
+  contentAlpha.innerHTML = data.values.alpha;
+  contentBeta.innerHTML = data.values.beta;
+  contentGamma.innerHTML = data.values.gamma;
+  dot.setAttribute("style", "left:" + data.values.x  + "%;" + "top:" + data.values.y + "%;");
+}
